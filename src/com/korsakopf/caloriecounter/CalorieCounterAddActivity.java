4503fbb231bc4eee47f5d6a869ca58aa7fa65f45
Anne de Boer <annedeboer@gmail.com>
2012-01-12 15:17:03 +0100

    Commit Toast

--- src/com/korsakopf/caloriecounter/CalorieCounterAddActivity.java
+++ src/com/korsakopf/caloriecounter/CalorieCounterAddActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 import com.korsakopf.caloriecounter.database.CalorieCounterDbAdapter;
 
 public class CalorieCounterAddActivity extends Activity {
-	
+
 	CalorieCounterDbAdapter db = new CalorieCounterDbAdapter(this);
 
 	EditText textNutName;
@@ -31,8 +31,6 @@ public class CalorieCounterAddActivity extends Activity {
 	int month;
 	int day;
 	String stringDate;
-	
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +45,6 @@ public class CalorieCounterAddActivity extends Activity {
 		buttonBack.setOnClickListener(buttonBackListener);
 	}
 
-
 	// Voeg aan de tabel toe als op de 'buttonAdd' wordt geklikt
 	private OnClickListener buttonAddNutValuesListener = new OnClickListener() {
 		public void onClick(View v) {
@@ -57,53 +54,60 @@ public class CalorieCounterAddActivity extends Activity {
 				// Datum van de datePicker widget omzetten naar een String
 				DatePicker = (DatePicker) findViewById(R.id.datePicker);
 				year = DatePicker.getYear();
-				month = DatePicker.getMonth()+1;
+				month = DatePicker.getMonth() + 1;
 				day = DatePicker.getDayOfMonth();
 				Date date = new Date((year - 1900), month, day);
 				String stringDate = date.toString();
 				long longDate = date.getTime();
 				String stringDate2 = "" + (day) + (month);
 				int intDate = Integer.parseInt(stringDate2);
-				
-				
 
 				// Edittext fields verbinden aan hun resource
 				textNutName = (EditText) findViewById(R.id.textNutName);
 				textNutGram = (EditText) findViewById(R.id.textNutGram);
 				textNutCal = (EditText) findViewById(R.id.textNutCal);
 				textNutProt = (EditText) findViewById(R.id.textNutProt);
-				
-				
-				
-					if (textNutName.getText().toString().length() != 0){
-						
-				// Roep de 'insertMultiple' method uit de DbAdapter aan 
-				db.insertMultiple(intDate, textNutName.getText().toString(),
-						Double.parseDouble(textNutGram.getText().toString()),
-						Double.parseDouble(textNutCal.getText().toString()),
-						Double.parseDouble(textNutProt.getText().toString()));
 
-				// Toast message die aangeeft wat en hoeveel is toegevoegd
-				id = db.getAllEntries();
-				Context context = getApplicationContext();
-				CharSequence text = "Het etenswaar '" + textNutName.getText()
-						+ "' is succesvol toegevoegd\nTotaal aantal rijen in de database is nu: " + id;
-				int duration = Toast.LENGTH_LONG;
-				Toast toast = Toast.makeText(context, text, duration);
-				toast.show();
+				// Controle of alle velden zijn ingevuld, zo niet dan een Toast melding
+				if (textNutName.getText().toString().equals("")
+						|| textNutGram.getText().equals("")
+						|| textNutCal.getText().equals("")
+						|| textNutProt.getText().equals("")) {
+					CharSequence ToastFieldsEmpty = "Vul alle velden in";
+					Context context = getApplicationContext();
+					int duur = Toast.LENGTH_LONG;
+					Toast toast2 = Toast.makeText(context, ToastFieldsEmpty, duur);
+					toast2.show();
+				} else {
 
-				// De textvelden weer leegmaken na het toevoegen aan de database
-				textNutName.setText("");
-				textNutGram.setText("");
-				textNutCal.setText("");
-				textNutProt.setText("");
-					}else{
-						Context context = getApplicationContext();
-						CharSequence melding = "test toast";
-						int duur = Toast.LENGTH_LONG;
-						Toast toast2 = Toast.makeText(context, melding, duur);
-						toast2.show();
-					}
+					// Roep de 'insertMultiple' method uit de DbAdapter aan
+					db.insertMultiple(intDate,
+							textNutName.getText().toString(), Double
+									.parseDouble(textNutGram.getText()
+											.toString()), Double
+									.parseDouble(textNutCal.getText()
+											.toString()), Double
+									.parseDouble(textNutProt.getText()
+											.toString()));
+
+					// Toast message die aangeeft wat en hoeveel is toegevoegd
+					id = db.getAllEntries();
+					Context context = getApplicationContext();
+					CharSequence text = "Het etenswaar '"
+							+ textNutName.getText()
+							+ "' is succesvol toegevoegd\nTotaal aantal rijen in de database is nu: "
+							+ id;
+					int duration = Toast.LENGTH_LONG;
+					Toast toast = Toast.makeText(context, text, duration);
+					toast.show();
+
+					// De textvelden weer leegmaken na het toevoegen aan de
+					// database
+					textNutName.setText("");
+					textNutGram.setText("");
+					textNutCal.setText("");
+					textNutProt.setText("");
+				}
 			} catch (Exception ex) {
 			}
 			db.close();
